01ea807be3d89398ddb2a9fa4fcfb8614ebd52e5
Jason Melnikel <jmelnikel@gmail.com>
2020-12-29 18:19:48 -0500

    Button Component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,18 @@
 import React from "react";
-
 import "components/Button.scss";
 
-export default function Button(props) {
-   return <></>;
+const Button = (props) => {
+  let buttonClass = "button"
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  } else if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+  return (
+    <button className={buttonClass} {props.disabled}>{props.children}</button>
+  );
 }
+
+export default Button;
