8365d460ef7c51af2e1560ed9cfdbb7894ffb3d9
Jason Melnikel <jmelnikel@gmail.com>
2020-12-30 13:20:37 -0500

    implement classnames library

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "components/Button.scss";
+import classnames from 'classnames';
 
 const Button = (props) => {
-  let buttonClass = "button"
-
-  if (props.confirm) {
-    buttonClass += " button--confirm"
-  }
-  if (props.danger) {
-    buttonClass += " button--danger"
-  }
+  const buttonClass = classnames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
 
   return (
     <button
