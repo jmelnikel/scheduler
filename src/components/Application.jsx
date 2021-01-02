c8b9a5a28bfe6824e0b52f83bb7113e0831a521e
Jason Melnikel <jmelnikel@gmail.com>
2021-01-02 15:03:35 -0500

    implement axios GET days

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,26 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Application.scss';
 import DayList from './DayList';
 import 'components/Appointment';
 import Appointment from './Appointment';
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-]
 
 const appointments = [
   {
@@ -71,7 +54,15 @@ const appointments = [
 
 
 const Application = () => {
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState("Monday");
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then((response) => {
+      const newDays = response.data
+      setDays(newDays)
+    });
+  }, [])
 
   return (
     <main className="layout">
