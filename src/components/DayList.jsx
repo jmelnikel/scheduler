46a8a471294132c38ffb513010daa36ee26d23a7
Jason Melnikel <jmelnikel@gmail.com>
2021-06-29 15:35:43 -0400

    implement DayList with Items

---
+++ src/components/DayList.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import DayListItem from './DayListItem'
+
+const DayList = (props) => {
+  const {
+    days,
+    day,
+    setDay,
+  } = props;
+
+  const DayListItemList = days.map((item) => {
+    const { name, spots } = item;
+    return (
+      <DayListItem
+        name={name}
+        spots={spots}
+        selected={name === day}
+        setDay={setDay}
+      />
+    )
+  })
+
+  return (
+    <ul>
+      {DayListItemList}
+    </ul>
+  )
+}
+
+DayList.propTypes = {
+  days: PropTypes.array,
+  day: PropTypes.string,
+  setDay: PropTypes.func,
+}
+
+export default DayList
+
---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import "components/DayListItem.scss";
+
+const DayListItem = (props) => {
+  const {
+    name,
+    spots,
+    selected,
+    setDay
+  } = props;
+
+  const dayClass = classnames(
+    'day-list__item',
+    {
+      'day-list__item--selected': selected,
+      'day-list__item--full': spots === 0,
+    }
+  )
+
+  const formatSpots = (spots) => {
+    if (spots === 0) return "no spots remaining"
+    else if (spots === 1) return "1 spot remaining"
+    else return `${spots} spots remaining`
+  }
+
+  return (
+    <li className={dayClass} onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
+    </li>
+  )
+}
+
+DayListItem.propTypes = {
+  name: PropTypes.string,
+  spots: PropTypes.number,
+  selected: PropTypes.bool,
+  setDay: PropTypes.func,
+}
+
+export default DayListItem
+
